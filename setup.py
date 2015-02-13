e030098681c546c5f7f6362c73a27395178e858e
Praveen Kumar <praveen97uma@gmail.com>
2015-02-13 14:24:39 +0530

    setup: add required dependencies and configure params

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 from setuptools import setup
 
+
+REQUIREMENTS = [
+    'requests'
+]
+
+
+CLASSIFIERS = [
+
+    'Development Status :: 3 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: APIs',
+    'license :: MIT License',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    ]
+
 setup(name='hackerearth',
       version='0.3',
       description='Python client for HackerEarth Code Checker API v3',
@@ -8,4 +24,6 @@ setup(name='hackerearth',
       author_email='praveen@hackerearth.com',
       license='MIT',
       packages=['hackerearth'],
-      zip_safe=False)
+      classifiers=CLASSIFIERS,
+      keywords='hackerarth code cheker api python client'
+      )
