d25fb4d5c7ef8d8d163bc734a72ec4793a72effd
Praveen Kumar <praveen97uma@gmail.com>
2015-02-13 13:46:59 +0530

    setup: update package information

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hackerearth',
+      version='0.3',
+      description='Python client for HackerEarth Code Checker API v3',
+      url='https://github.com/HackerEarth/he-sdk-python',
+      author='Praveen Kumar',
+      author_email='praveen@hackerearth.com',
+      license='MIT',
+      packages=['hackerearth'],
+      zip_safe=False)
