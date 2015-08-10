a7acf0416e3d38585b74ce292d7203415cfc1960
Vivek Prakash <vivek@hackerearth.com>
2015-08-10 14:59:44 +0530

    he-sdk: update test api code for unicode support

--- hackerearth/test_api.py
+++ hackerearth/test_api.py
@@ -1,27 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+from parameters import SupportedLanguages
+from parameters import RunAPIParameters
 
-from hackerearth.parameters import SupportedLanguages
-from hackerearth.parameters import RunAPIParameters
-
-from hackerearth.api_handlers import HackerEarthAPI
+from api_handlers import HackerEarthAPI
 
 client_secret = '0a7f0101e5cc06e4417a3addeb76164680ac83a4'
-source = "print 23"
+
+source = open('test_source.py', 'r').read()
 lang = SupportedLanguages.PYTHON
 compressed = 1
 html = 0
-params = RunAPIParameters(client_secret=client_secret, source=source,
-        lang='PYTHON', compressed=compressed, html=0)
+params = RunAPIParameters(
+        client_secret=client_secret, source=source,
+        lang=lang, compressed=compressed, html=html)
 
 api = HackerEarthAPI(params)
 
+print 'Compiling code..'
 r = api.compile()
-print r.async
-print r.compile_status
-print r.web_link
-print r.code_id
-print r.id
 print r.__dict__
 
+print '\nRunning code...'
 r = api.run()
 print r.__dict__
+output = r.__dict__.get('output')
+
+print '\nRun Output:'
+print output
---
+++ hackerearth/test_source.py
@@ -0,0 +1,11 @@
+print 'Hello World in English...'
+print 'Hello World\n'
+
+print 'Hello World in Chinese...'
+print '你好世界\n'
+
+print 'Hello World in Polish...'
+print 'Witaj świecie\n'
+
+print 'Hello World in Russian...'
+print 'Привет мир\n'
